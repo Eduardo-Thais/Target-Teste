22d3e604f41fcb094a3e97c1ce125d2331b8fdba
Eduardo de Souza Santos Thais <33501705+Eduardo-Thais@users.noreply.github.com>
2025-05-13 11:57:46 -0300

    primeiro commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Target_Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
